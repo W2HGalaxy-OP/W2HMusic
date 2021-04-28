5b58d20e650cdf8980b6ebd21432f5b04354e5d7
W2H Galaxy <w2hgalaxy@outlook.com>
2021-04-28 12:42:04 +0530

    Initial commit

---
+++ handlers/start.py
@@ -0,0 +1,54 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @AuraXSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Support⚡️", url="https://t.me/AuraXSupport"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Creater⚡️", url="https://t.me/AuraX_Owner"
+                    ),
+                    InlineKeyboardButton(
+                        "PglZone⚡️", url="https://t.me/PglZone"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Add To Your Group⚡️", url="https://t.me/AuraXMusicBot?startgroup=true"
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
