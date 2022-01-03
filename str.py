758286b599f7720cfccfdac1f17ab84ebd5ac88c
W2H Galaxy <82048990+W2HGalaxy-OP@users.noreply.github.com>
2022-01-03 14:17:04 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,16 +1,21 @@
 import asyncio
+from pyrogram import Client as c
 
-from pyrogram import Client
+API_ID = input("\nEnter Your API_ID:\n > ")
+API_HASH = input("\nEnter Your API_HASH:\n > ")
 
+print("\n\n Enter Phone number when asked.\n\n")
 
-print("Enter your app information from my.telegram.org/apps below.")
+i = c(":memory:", api_id=API_ID, api_hash=API_HASH)
 
 
 async def main():
-    async with Client(":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")) as app:
-        print(await app.export_session_string())
+  await i.start()
+  i.storage.SESSION_STRING_FORMAT = ">B?256sQ?"
+  i.storage.SESSION_STRING_SIZE = 356
+  ss = await i.export_session_string()
+  print("\nHERE IS YOUR STRING SESSION, COPY IT, DON'T SHARE For Support Join @w2hsupport !!\n")
+  print(f"\n{ss}\n")
 
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+asyncio.run(main())
