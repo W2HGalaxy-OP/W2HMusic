c0c7f6a4f9cab74ef1a9969d9e481a283222d14a
Qwerty25772 <100129239+Qwerty25772@users.noreply.github.com>
2022-08-17 09:55:58 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,25 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @xsolider.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "Support⚡️", url="https://t.me/xsolider"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/David99q"
+                        "Creater⚡️", url="https://t.me/Dark_295"
                     ),
                     InlineKeyboardButton(
-                        "Ravan102030⚡️", url="https://t.me/Ravan102030"
+                        "xsolider⚡️", url="https://@Dark_295"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/W2H_Music_1_Bot?startgroup=true"
+                        "Add To Your Group⚡️", url="https://t.me/xsolider?startgroup=true"
                     )
                 ]
             ]
